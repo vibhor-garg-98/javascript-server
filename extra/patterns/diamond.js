0b6f882b0da1dea1ca555baa0ee5ae7962eea6e7
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-01-24 11:42:13 +0530

    feat:patterns

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -1,36 +1,47 @@
-let r=Number(process.argv[2]);
-diamond(r);
+let r = Number(process.argv[2]);
+
 function diamond(n) 
 {
-  if(n<11 && n>1)
-  {
-    for(let i=0;i<n;i++)
+    if( n<11 && n>1 )
     {
-      let row =" ";
-      for(let j=0;j<n-i;j++)
-      {
-        row=row+" ";
-      }
-      for(let k=0;k<=i;k++)
-      {
-        row=row+"* ";
-      }
-      console.log(row);
-    }
-    for(let i=n-1;i>0;i--)
+
+       for( let i=0; i<n; i++ )
+       {
+          let row = " ";
+
+          for( let j=0; j<n-i; j++ )
+          {
+             row = row + " ";
+          }
+
+          for( let k=0; k<=i; k++ )
+          {
+             row = row + "* ";
+          }
+
+          console.log(row);
+       }
+
+       for( let i=n-1; i>0; i-- )
+       {
+          let row = " ";
+
+          for( let j=0; j<n-i; j++ )
+          {
+             row = row + " ";
+          }
+
+          for( let k=0; k<i; k++ )
+          {
+             row = row + " *";
+          }
+
+          console.log(row);    
+       }
+    } 
+    else
     {
-      let row =" ";
-      for(let j=0;j<n-i;j++)
-      {
-        row=row+" ";
-      }
-      for(let k=0;k<i;k++)
-      {
-        row=row+" *";
-      }
-      console.log(row);    }
-  } 
-  else{
       console.log("wrong input");
-  }
+    }
 }
+diamond(r);
--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,24 +1,30 @@
-let r=Number(process.argv[2]);
-equilateral(r);
+let r = Number(process.argv[2]);
+
 function equilateral(n) 
 {
-  if(n<11 && n>1)
-  {
-    for(let i=0;i<n;i++)
+  if( n<11 && n>1 )
+  { 
+    
+    for( let i=0; i<n; i++ )
     {
-      let row =" ";
-      for(let j=0;j<n-i;j++)
+      let row = " ";
+
+      for( let j=0; j<n-i; j++ )
       {
-        row=row+" ";
+        row = row + " ";
       }
-      for(let k=0;k<=i;k++)
+
+      for( let k=0; k<=i; k++ )
       {
-        row=row+"* ";
+        row = row + "* ";
       }
+
       console.log(row);
     }
   } 
-  else{
-      console.log("wrong input");
+  else
+  {
+   console.log("wrong input");
   }
 }
+equilateral(r);
