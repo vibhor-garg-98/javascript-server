820436e54ef98aa02dd692dd635055502bc7ec6c
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-01-27 19:29:00 +0530

    feat:add packages

--- extra/utils/helpers.js
+++ extra/utils/helpers.js
@@ -2,11 +2,6 @@ function validateEmail(email)
 {
     const re =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((successive.tech))$/;
     let x = re.test(email);
-
-    // if( x == true ) 
-    //     valid.push(email);
-    // else 
-    //     invalid.push(email);
     
     return(x);
 }
--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -8,26 +8,31 @@ import {validateEmail} from"./helpers";
 export default function validateUsers(users){
 users.forEach((element) => {
 const {traineeEmail, reviewerEmail} = element;
+
 let check = validateEmail(traineeEmail);
-if( check == true){
-validcount++;
-valid.push(traineeEmail);
+
+if( check == true)
+{
+  validcount++;
+  valid.push(traineeEmail);
 }
-else{
-invalidcount++;
-invalid.push(traineeEmail);
+else
+{
+  invalidcount++;
+  invalid.push(traineeEmail);
 }
 let check1 = validateEmail(reviewerEmail);
-if( check1 == true){
-validcount++;
-valid.push(reviewerEmail);
+if( check1 == true)
+{
+  validcount++;
+  valid.push(reviewerEmail);
 }
-else{
-invalidcount++;
-invalid.push(reviewerEmail);
+else
+{
+  invalidcount++;
+  invalid.push(reviewerEmail);
 }
 })
-
 
 console.log("valid count \n"+validcount);
 console.log ("valid email\n" +valid);
