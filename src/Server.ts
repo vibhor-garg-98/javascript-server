55c3c97157672c6014a16f7811238e24b8fca39b
vibhor-garg-98 <vibhor.garg@successive.tech>
2020-01-30 15:55:08 +0530

    feat: add handlers and middleware

--- src/Server.ts
+++ src/Server.ts
@@ -1,41 +1,80 @@
 import * as express from 'express';
-import IConfig from './config/IConfig';
+import Iconfig from './config/IConfig';
+import * as bodyParser from 'body-parser';
+import errorHandler from './libs/routes/errorHandler';
+import notFoundRoutes from './libs/routes/notFoundRoute';
+import { Request } from 'express';
+
+interface User {
+  name: string;
+  id: string;
+}
+
+interface NewRequest extends Request {
+  user: User;
+}
 
 class Server {
   private app: express.Express;
 
-  constructor(private config : IConfig) {
+  constructor(private config: Iconfig) {
     this.app = express();
   }
 
-  bootstrap = () => {
-    console.log('Inside bootsrap');
+  bootstrap = (): Server=> {
+    console.log('Inside Bootstrap');
+    this.initBodyParser();
     this.setupRoutes();
     return this;
   };
-  run = () => {
+
+  initBodyParser = () : void => {
+    const { app } = this;
+
+    console.log('Inside initBodyParser');
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.use(bodyParser.json());
+  };
+
+  run = () :void => {
     const {
       app,
       config: { port }
     } = this;
 
-    app.listen(port, err => {
+    this.app.listen(this.config.port, err => {
       if (err) {
+        console.log('error');
         throw err;
       }
       console.log('App is running successfully on port ' + port);
     });
   };
 
-  setupRoutes = () => {
+  setupRoutes = () : Server => {
     const { app } = this;
 
-    app.get('/health-check', (req, res) => {
-      console.log('Inside health check');
-      res.send('i am ok');
+    this.app.get('/health-check', (req: express.Request, res: express.Response ) => {
+        console.log('Inside health check');
+        res.send('I am OK');
+      }
+    );
+
+    app.use('/api', (req: NewRequest, res : any, next : any) => {
+      console.log('Inside Middleware');
+      req.user = {
+        id: '1',
+        name: 'Node'
+      };
+      console.log(req.user);
+      res.send('ok');
     });
+
+    app.use(notFoundRoutes);
+    app.use(errorHandler);
     return this;
   };
 }
-
 export default Server;
---
+++ src/libs/routes/errorHandler.ts
@@ -0,0 +1,10 @@
+const errorHandler = (err : any, req : any, res: any, next: any) => {
+  console.log('Error', err);
+  res.send({
+    error: err.error,
+    message: err.error,
+    status: err.error,
+    timestamp: new Date()
+  });
+};
+export default errorHandler;
